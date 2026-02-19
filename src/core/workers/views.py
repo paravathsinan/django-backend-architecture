b0b859813d467daededa11df2266c44e5635735e
Sinan Paravath <paravathsinan@gmail.com>
2026-02-19 17:09:19 +0530

    csrf removed - views.py

--- src/core/workers/views.py
+++ src/core/workers/views.py
@@ -3,8 +3,6 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .serializers import WorkerSerializer
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.decorators.csrf import csrf_protect
-from django.utils.decorators import method_decorator
 from .models import Worker
 from .forms import WorkerForm
 
@@ -63,7 +61,7 @@ def worker_api(request):
         return Response(serializer.error, status=400)
 '''
 
-@method_decorator(csrf_protect, name='dispatch')
+                                                                                                                                
 class WorkerViewSet(ModelViewSet):
     queryset = Worker.objects.all()
     serializer_class = WorkerSerializer
